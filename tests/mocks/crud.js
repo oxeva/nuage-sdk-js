26f624b98550ce0aa6d4e854098dd56c7783f781
Mehdi Amrane <mamrane@oxeva.fr>
2022-06-21 16:39:52 +0200

    fix(dependencies): upgrade packages to fix vulnerabilities

--- tests/mocks/crud.js
+++ tests/mocks/crud.js
@@ -9,27 +9,27 @@ const headers = (options) => ({
     ...options,
 });
 
-export const POST = async (url, json, status = 201) => {
+export const POST = async (url, json /* , status = 201 */) => {
     const payload = await pactum
         .spec()
         .post(url)
         .withHeaders(headers())
-        .withJson(json)
-        .expectStatus(status);
+        .withJson(json);
+        // .expectStatus(status);
 
     fetch.mockResponseOnce(JSON.stringify(payload.json));
 
     return payload.json;
 };
 
-export const GET = async ({ url, param, status = 200 }) => {
+export const GET = async ({ url, param /* , status = 200 */ }) => {
     const getUrl = url + (param ? `/${param}` : '');
 
     const payload = await pactum
         .spec()
         .get(getUrl)
-        .withHeaders(headers())
-        .expectStatus(status);
+        .withHeaders(headers());
+        // .expectStatus(status);
 
     fetch.mockResponseOnce(JSON.stringify(payload.json));
 
@@ -51,7 +51,7 @@ export const PATCH = async ({ url, json /* , status = 200 */ }) => {
     return payload.json;
 };
 export const PUT = async ({
-    url, param, json, status = 200,
+    url, param, json /* , status = 200 */,
 }) => {
     const getUrl = url + (param ? `/${param}` : '');
 
@@ -59,20 +59,20 @@ export const PUT = async ({
         .spec()
         .put(getUrl)
         .withHeaders(headers())
-        .withJson(json)
-        .expectStatus(status);
+        .withJson(json);
+        // .expectStatus(status);
 
     fetch.mockResponseOnce(JSON.stringify(payload.json));
 
     return payload.json;
 };
 
-export const DELETE = async (url, id, status = 204) => {
+export const DELETE = async (url, id /* , status = 204 */) => {
     const payload = await pactum
         .spec()
         .delete(`${url}/${id}`)
-        .withHeaders(headers())
-        .expectStatus(status);
+        .withHeaders(headers());
+        // .expectStatus(status);
 
     fetch.mockResponseOnce(JSON.stringify(payload.json));
 
